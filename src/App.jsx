2509fb6c50e66e771f8fc86b158dea352fa83870
Yashpriyadeep Katta <yashpriyadeepkatta@Yashpriyadeeps-MacBook-Air.local>
2025-06-11 09:20:48 +0530

    home-page-work

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { NavBar } from "./components/navbar/navbar"
+import { HomePage } from "./components/homepage/homepage";
 import { NavLink } from "react-router-dom";
 
 function App() {
 
   return <div>
     <NavBar />
+    <HomePage />
   </div>
 }
 
---
+++ src/components/homepage/homepage.jsx
@@ -0,0 +1,16 @@
+import "./homepage.css"
+
+export function HomePage() {
+    return <div className="homepage">
+        <div className="homepage-container">
+            <img className="homepage-banner-img" src="././assets/images/logo.png" alt="" />
+        </div>
+        <div className="homepage-about-cosmos">
+            <img className="homepage-astro-left" src="././assets/images/astronaut-64.png" alt="" />
+
+            <img className="homepage-astro-right" src="././assets/images/astronaut-right.png" alt="" />
+        </div>
+
+
+    </div>
+}
